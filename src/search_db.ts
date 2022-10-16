d80e02c0eabd80beda3a9784573d2c16d51a28f4
David Rojo <davidrojo1997@gmail.com>
2022-10-17 00:04:32 +0200

    feat: handle errors, remove unnecessary await, format code

--- src/search_db.ts
+++ src/search_db.ts
@@ -1,44 +1,59 @@
-require('dotenv').config();
-const { MongoClient } = require('mongodb');
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 
-const url = process.env.MONGODB_LINK ?? '';
-const dbName = 'yasse';
+const url = process.env.MONGODB_LINK ?? "";
+const dbName = "yasse";
 
 /**
-* Creates ideal string to make regex search in MongoDB.
-* @param {string} searchTerm - String to be formatted.
-* @return {string} Formatted string.
-*/
-export function formatSearch( searchTerm: string ): string {
-    const formattedSearch: string = searchTerm.split( '' ).join( '.*' );
+ * Creates ideal string to make regex search in MongoDB.
+ * @param {string} searchTerm - String to be formatted.
+ * @return {string} Formatted string.
+ */
+export function formatSearch(searchTerm: string): string {
+  if (typeof searchTerm !== "string") {
+    throw new Error("Search term must be a string.");
+  }
 
-    return formattedSearch;
+  return searchTerm.split("").join(".*");
 }
 
 /**
-* Searches for title in collectionToSearch.
-* @param {string} title - Term to be searched.
-* @param {string} collectionToSearch - Collection to search.
-* @param {boolean} [orderByLatestChapter] - Order search or not.
-* @return List of JSON document with search results.
-*/
-export async function searchDB( title: string, collectionToSearch: string, orderByLatestChapter?: boolean ) {
-    const client = await MongoClient.connect( url );
-    const db = client.db( dbName );
-    const collection = db.collection( collectionToSearch );
-    
-    const result_search = await collection.find(
-        { 'title': { $regex: `.*${ title.toLowerCase() }.*` } },
-        { projection: { _id: 0 } }
-    );
-    
-    const result = orderByLatestChapter ?
-        await result_search.sort({ latestChapter: -1 }).toArray() :
-        await result_search.toArray();
+ * Searches for title in collectionToSearch.
+ * @param {string} title - Term to be searched.
+ * @param {string} collectionToSearch - Collection to search.
+ * @param {boolean} [orderByLatestChapter] - Order search or not.
+ * @return List of JSON document with search results.
+ */
+export async function searchDB(
+  title: "anime" | "manga" | "webtoon" | "novel",
+  collectionToSearch: string,
+  orderByLatestChapter?: boolean
+) {
+  let client;
+  try {
+    client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collectionToSearch);
 
-    await client.close();
-    
+    if (!collection) {
+      throw new Error("Collection not found.");
+    }
+
+    const searchResult = await collection.find(
+      { title: { $regex: `.*${title.toLowerCase()}.*` } },
+      { projection: { _id: 0 } }
+    );
+
+    const result = orderByLatestChapter
+      ? searchResult.sort({ latestChapter: -1 }).toArray()
+      : searchResult.toArray();
+
     return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.close();
+  }
 }
 
 module.exports = { formatSearch, searchDB };
