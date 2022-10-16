07917d57f76ef75eb1666c9ea256ba63b021e990
David Rojo <davidrojo1997@gmail.com>
2022-10-17 00:45:40 +0200

    refactor: unify code style

--- src/api.ts
+++ src/api.ts
@@ -1,37 +1,37 @@
-const express = require("express");
-const { searchDB } = require("./search_db");
+const express = require('express');
+const { searchDB } = require('./search_db');
 
 export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/anime", async (req, res) => {
+app.get('/anime', async (req, res) => {
   const title = req.query.title;
   const orderByLatestChapter = req.query.orderByLatestChapter;
 
-  res.send(searchDB(title.toString(), "anime", !!orderByLatestChapter));
+  res.send(searchDB(title.toString(), 'anime', !!orderByLatestChapter));
 });
 
-app.get("/manga", async (req, res) => {
+app.get('/manga', async (req, res) => {
   const title = req.query.title;
   const orderByLatestChapter = req.query.orderByLatestChapter;
 
-  res.send(searchDB(title.toString(), "manga", !!orderByLatestChapter));
+  res.send(searchDB(title.toString(), 'manga', !!orderByLatestChapter));
 });
 
-app.get("/webtoon", async (req, res) => {
+app.get('/webtoon', async (req, res) => {
   const title = req.query.title;
   const orderByLatestChapter = req.query.orderByLatestChapter;
 
-  res.send(searchDB(title.toString(), "webtoon", !!orderByLatestChapter));
+  res.send(searchDB(title.toString(), 'webtoon', !!orderByLatestChapter));
 });
 
-app.get("/novel", async (req, res) => {
+app.get('/novel', async (req, res) => {
   const title = req.query.title;
   const orderByLatestChapter = req.query.orderByLatestChapter;
 
-  res.send(searchDB(title.toString(), "novel", !!orderByLatestChapter));
+  res.send(searchDB(title.toString(), 'novel', !!orderByLatestChapter));
 });
 
 module.exports = { app };
--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ const app = require('./api');
 
 const PORT = 3000;
 
-app.listen( PORT );
+app.listen(PORT);
