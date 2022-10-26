38e5ca8767460472288369e701634d68ebcfacff
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2022-10-26 10:30:00 +0530

    404 and 500 Error Handling

--- src/routes.ts
+++ src/routes.ts
@@ -8,47 +8,68 @@ router.use(cors());
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-const errorHandler = async (req, res) => {
+router.get('/anime', async (req, res) => {
+    const title = req.query.title ?? '';
+    const orderByLatestChapter = req.query.orderByLatestChapter ?? false;
+
     try {
-        const result = await req();
-        if (result) {
+        const result = await searchDB(title.toString(), 'anime', !!orderByLatestChapter);
+        if (result && result.length > 0) {
             return res.status(200).send(result);
-        } else {
+        } else if (result && result.length === 0) {
             return res.status(404).send('Not found');
         }
     } catch (error) {
         return res.status(500).send('Internal server error');
     }
-}
-
-router.get('/anime', async (req, res) => {
-    const title = req.query.title ?? '';
-    const orderByLatestChapter = req.query.orderByLatestChapter ?? false;
-
-    errorHandler(searchDB(title.toString(), 'anime', !!orderByLatestChapter), res);
 });
 
 router.get('/manga', async (req, res) => {
     const title = req.query.title ?? '';
     const orderByLatestChapter = req.query.orderByLatestChapter;
 
-    errorHandler(searchDB(title.toString(), 'manga', !!orderByLatestChapter), res);
+    try {
+        const result = await searchDB(title.toString(), 'manga', !!orderByLatestChapter);
+        if (result && result.length > 0) {
+            return res.status(200).send(result);
+        } else if (result && result.length === 0) {
+            return res.status(404).send('Not found');
+        }
+    } catch (error) {
+        return res.status(500).send('Internal server error');
+    }
 });
 
 router.get('/webtoon', async (req, res) => {
     const title = req.query.title ?? '';
     const orderByLatestChapter = req.query.orderByLatestChapter;
 
-    errorHandler(
-        searchDB(title.toString(), 'webtoon', !!orderByLatestChapter), res
-    );
+    try {
+        const result = await searchDB(title.toString(), 'webtoon', !!orderByLatestChapter);
+        if (result && result.length > 0) {
+            return res.status(200).send(result);
+        } else if (result && result.length === 0) {
+            return res.status(404).send('Not found');
+        }
+    } catch (error) {
+        return res.status(500).send('Internal server error');
+    }
 });
 
 router.get('/novel', async (req, res) => {
     const title = req.query.title ?? '';
     const orderByLatestChapter = req.query.orderByLatestChapter;
 
-    errorHandler(searchDB(title.toString(), 'novel', !!orderByLatestChapter), res);
+    try {
+        const result = await searchDB(title.toString(), 'novel', !!orderByLatestChapter);
+        if (result && result.length > 0) {
+            return res.status(200).send(result);
+        } else if (result && result.length === 0) {
+            return res.status(404).send('Not found');
+        }
+    } catch (error) {
+        return res.status(500).send('Internal server error');
+    }
 });
 
 export default router;
