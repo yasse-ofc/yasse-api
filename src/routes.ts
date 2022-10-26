9b15e65fb4c2213cf77f420d069f2da2a85f5b2a
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2022-10-26 10:02:34 +0530

    Added 404 and 500 error handling

--- src/routes.ts
+++ src/routes.ts
@@ -8,26 +8,39 @@ router.use(cors());
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
+const errorHandler = async (req, res) => {
+    try {
+        const result = await req();
+        if (result) {
+            return res.status(200).send(result);
+        } else {
+            return res.status(404).send('Not found');
+        }
+    } catch (error) {
+        return res.status(500).send('Internal server error');
+    }
+}
+
 router.get('/anime', async (req, res) => {
     const title = req.query.title ?? '';
     const orderByLatestChapter = req.query.orderByLatestChapter ?? false;
 
-    res.send(await searchDB(title.toString(), 'anime', !!orderByLatestChapter));
+    errorHandler(searchDB(title.toString(), 'anime', !!orderByLatestChapter), res);
 });
 
 router.get('/manga', async (req, res) => {
     const title = req.query.title ?? '';
     const orderByLatestChapter = req.query.orderByLatestChapter;
 
-    res.send(await searchDB(title.toString(), 'manga', !!orderByLatestChapter));
+    errorHandler(searchDB(title.toString(), 'manga', !!orderByLatestChapter), res);
 });
 
 router.get('/webtoon', async (req, res) => {
     const title = req.query.title ?? '';
     const orderByLatestChapter = req.query.orderByLatestChapter;
 
-    res.send(
-        await searchDB(title.toString(), 'webtoon', !!orderByLatestChapter)
+    errorHandler(
+        searchDB(title.toString(), 'webtoon', !!orderByLatestChapter), res
     );
 });
 
@@ -35,7 +48,7 @@ router.get('/novel', async (req, res) => {
     const title = req.query.title ?? '';
     const orderByLatestChapter = req.query.orderByLatestChapter;
 
-    res.send(await searchDB(title.toString(), 'novel', !!orderByLatestChapter));
+    errorHandler(searchDB(title.toString(), 'novel', !!orderByLatestChapter), res);
 });
 
 export default router;
